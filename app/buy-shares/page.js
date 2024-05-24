3085239b200f02c1af89158179a387728d6be2b8
Balaj Ali <balaj.ali707@gmail.com>
2024-05-24 18:54:50 +0500

    buy-shares page view created and controllers integrated

--- app/buy-shares/page.js
+++ app/buy-shares/page.js
@@ -6,6 +6,10 @@ import SearchBar from "@/components/buy-shares/searchBar";
 import FilterComponent from "@/components/buy-shares/filterComponent";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Image from "next/image";
+import { VscEye } from "react-icons/vsc";
+import { FiMapPin } from "react-icons/fi";
+import compCities from "countrycitystatejson";
 
 const Page = () => {
   const dispatch = useDispatch();
@@ -37,9 +41,12 @@ const Page = () => {
   );
 
   const [featuredProperties, setFeaturedProperties] = useState([]);
+  const [isFeaturedPropertyLoading, setIsFeaturedPropertyLoading] =
+    useState(true);
 
   const handleFetchFeaturedProperty = async () => {
     try {
+      setIsFeaturedPropertyLoading(true);
       const res = await fetch(
         `${
           process.env.NEXT_PUBLIC_SERVER_HOST
@@ -60,9 +67,15 @@ const Page = () => {
       );
 
       const response = await res.json();
-
-      console.log("response: ", response);
+      if (response.success) {
+        setIsFeaturedPropertyLoading(false);
+        setFeaturedProperties(response.body);
+      } else {
+        throw new Error(response.message);
+      }
     } catch (error) {
+      setIsFeaturedPropertyLoading(false);
+
       toast.error(error.message, {
         position: "bottom-center",
         autoClose: 5000,
@@ -77,9 +90,11 @@ const Page = () => {
   };
 
   const [mostViewedProperties, setMostViewedProperties] = useState([]);
+  const [isMostViewedLoading, setIsMostViewedLoading] = useState(true);
 
   const handleFetchMostViewedProperty = async () => {
     try {
+      setIsMostViewedLoading(true);
       const res = await fetch(
         `${
           process.env.NEXT_PUBLIC_SERVER_HOST
@@ -100,9 +115,14 @@ const Page = () => {
       );
 
       const response = await res.json();
-
-      console.log("response: ", response);
+      if (response.success) {
+        setIsMostViewedLoading(false);
+        setMostViewedProperties(response.body);
+      } else {
+        throw new Error(response.message);
+      }
     } catch (error) {
+      setIsMostViewedLoading(false);
       toast.error(error.message, {
         position: "bottom-center",
         autoClose: 5000,
@@ -117,9 +137,11 @@ const Page = () => {
   };
 
   const [recentlyAddedProperties, setRecentlyAddedProperties] = useState([]);
+  const [isRecentlyAddedLoading, setIsRecentlyAddedLoading] = useState(true);
 
   const handleFetchRecentlyAddedProperty = async () => {
     try {
+      setIsRecentlyAddedLoading(true);
       const res = await fetch(
         `${
           process.env.NEXT_PUBLIC_SERVER_HOST
@@ -140,9 +162,14 @@ const Page = () => {
       );
 
       const response = await res.json();
-
-      console.log("response: ", response);
+      if (response.success) {
+        setIsRecentlyAddedLoading(false);
+        setRecentlyAddedProperties(response.body);
+      } else {
+        throw new Error(response.message);
+      }
     } catch (error) {
+      setIsRecentlyAddedLoading(false);
       toast.error(error.message, {
         position: "bottom-center",
         autoClose: 5000,
@@ -173,6 +200,12 @@ const Page = () => {
       applyBtnRef.current.classList.add("z-0");
     }
   }, [isFilterUpdated]);
+
+  useEffect(() => {
+    handleFetchFeaturedProperty();
+    handleFetchMostViewedProperty();
+    handleFetchRecentlyAddedProperty();
+  }, []);
 
   return (
     <>
@@ -208,7 +241,282 @@ const Page = () => {
           Apply Changes
         </button>
       </div>
-      ;
+      {featuredProperties.length > 0 && (
+        <div className="my-10 ">
+          <h1 className="xl:text-[40px] text-4xl mb-5 text-center text-[#116A7B] font-semibold ">
+            Featured Properties
+          </h1>
+          {!isFeaturedPropertyLoading ? (
+            <div className="xl:mx-24 mx-16 flex flex-row flex-wrap space-x-20">
+              {featuredProperties.map((card, cardIndex) => (
+                <div
+                  key={cardIndex}
+                  className="w-[20rem] bg-white border-2 border-[#D9D9D9] rounded-xl"
+                >
+                  <div className="p-2 relative">
+                    <Image
+                      width={1000}
+                      height={1000}
+                      src={
+                        card.imageCount > 0
+                          ? `${process.env.NEXT_PUBLIC_SERVER_HOST}/uploads/${card.propertyID}/image-0.png`
+                          : "/assets/user/property-management/no-image.jpg"
+                      }
+                      className="w-[20rem] h-auto object-cover object-center rounded-md overflow-hidden"
+                      // alt={`${}`}
+                    />
+                    <span
+                      // onClick={() =>
+                      //   handleFavouriteList(
+                      //     slideIndex,
+                      //     cardIndex,
+                      //     !favourites[slideIndex][cardIndex]?.status
+                      //   )
+                      // }
+                      className="absolute inset-y-5 left-0 px-5 text-red-600 font-semibold focus:outline-none cursor-pointer"
+                    >
+                      {/* {favourites[slideIndex][cardIndex]?.status ? (
+                  <FaHeart />
+                ) : (
+                  <FaRegHeart />
+                )} */}
+                    </span>
+                    <span className="absolute text-xs inset-y-5 right-0 px-5 text-[#116A7B] font-semibold focus:outline-none cursor-pointer">
+                      {" "}
+                      <VscEye className="inline-flex mb-[1px] mr-1 text-base" />
+                      {card.viewedCount}{" "}
+                    </span>
+                  </div>
+                  <div className="p-2 space-y-1">
+                    <h1 className="text-xl text-[#116A7B]">
+                      {card.propertyType}
+                    </h1>
+                    <h2 className="text-sm text-[#116A7B]">
+                      <strong>
+                        {card?.amenities?.roomDetails?.inputs?.beds
+                          ? card?.amenities?.roomDetails?.inputs?.beds
+                          : "-"}
+                      </strong>{" "}
+                      bd{" "}
+                      <strong>
+                        {card?.amenities?.roomDetails?.inputs?.baths
+                          ? card?.amenities?.roomDetails?.inputs?.baths
+                          : "-"}
+                      </strong>{" "}
+                      ba <strong>{card.area}</strong> Sqft
+                    </h2>
+                    <h3 className="text-sm text-gray-300">
+                      <strong className="text-gray-900">
+                        {card.totalStakes - card.stakesOccupied}
+                      </strong>{" "}
+                      of {card.totalStakes} shares avi.
+                    </h3>
+                    <h4 className="text-xl flex items-start text-[#116A7B]">
+                      <FiMapPin className="inline-flex mt-1 mr-1" />{" "}
+                      {card.addressOfProperty.state},{" "}
+                      {
+                        compCities.getCountryByShort(
+                          card.addressOfProperty.country
+                        ).name
+                      }
+                    </h4>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="bg-white w-full my-6 h-[40rem] max-h-[44rem] overflow-y-auto flex flex-row items-center justify-center">
+              <div className="border-t-4 border-b-4 border-[#116A7B] bg-transparent h-20 p-2 m-3 animate-spin duration-[2200] shadow-lg w-20 mx-auto rounded-full"></div>
+            </div>
+          )}
+        </div>
+      )}
+      {mostViewedProperties.length > 0 && (
+        <div className="py-10 bg-[#116A7B]">
+          <h1 className="xl:text-[40px] text-4xl mb-16 text-center text-white font-semibold ">
+            Most Viewed Properties
+          </h1>
+          {!isMostViewedLoading ? (
+            <div className="xl:mx-24 mx-16 flex flex-row flex-wrap space-x-20">
+              {mostViewedProperties.map((card, cardIndex) => (
+                <div
+                  key={cardIndex}
+                  className="w-[20rem] bg-white border-2 border-[#D9D9D9] rounded-xl"
+                >
+                  <div className="p-2 relative">
+                    <Image
+                      width={1000}
+                      height={1000}
+                      src={
+                        card.imageCount > 0
+                          ? `${process.env.NEXT_PUBLIC_SERVER_HOST}/uploads/${card.propertyID}/image-0.png`
+                          : "/assets/user/property-management/no-image.jpg"
+                      }
+                      className="w-[20rem] h-auto object-cover object-center rounded-md overflow-hidden"
+                      // alt={`${}`}
+                    />
+                    <span
+                      // onClick={() =>
+                      //   handleFavouriteList(
+                      //     slideIndex,
+                      //     cardIndex,
+                      //     !favourites[slideIndex][cardIndex]?.status
+                      //   )
+                      // }
+                      className="absolute inset-y-5 left-0 px-5 text-red-600 font-semibold focus:outline-none cursor-pointer"
+                    >
+                      {/* {favourites[slideIndex][cardIndex]?.status ? (
+                  <FaHeart />
+                ) : (
+                  <FaRegHeart />
+                )} */}
+                    </span>
+                    <span className="absolute text-xs inset-y-5 right-0 px-5 text-[#116A7B] font-semibold focus:outline-none cursor-pointer">
+                      {" "}
+                      <VscEye className="inline-flex mb-[1px] mr-1 text-base" />
+                      {card.viewedCount}{" "}
+                    </span>
+                  </div>
+                  <div className="p-2 space-y-1">
+                    <h1 className="text-xl text-[#116A7B]">
+                      {card.propertyType}
+                    </h1>
+                    <h2 className="text-sm text-[#116A7B]">
+                      <strong>
+                        {card?.amenities?.roomDetails?.inputs?.beds
+                          ? card?.amenities?.roomDetails?.inputs?.beds
+                          : "-"}
+                      </strong>{" "}
+                      bd{" "}
+                      <strong>
+                        {card?.amenities?.roomDetails?.inputs?.baths
+                          ? card?.amenities?.roomDetails?.inputs?.baths
+                          : "-"}
+                      </strong>{" "}
+                      ba <strong>{card.area}</strong> Sqft
+                    </h2>
+                    <h3 className="text-sm text-gray-300">
+                      <strong className="text-gray-900">
+                        {card.totalStakes - card.stakesOccupied}
+                      </strong>{" "}
+                      of {card.totalStakes} shares avi.
+                    </h3>
+                    <h4 className="text-xl flex items-start text-[#116A7B]">
+                      <FiMapPin className="inline-flex mt-1 mr-1" />{" "}
+                      {card.addressOfProperty.state},{" "}
+                      {
+                        compCities.getCountryByShort(
+                          card.addressOfProperty.country
+                        ).name
+                      }
+                    </h4>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="bg-white w-full my-6 h-[40rem] max-h-[44rem] overflow-y-auto flex flex-row items-center justify-center">
+              <div className="border-t-4 border-b-4 border-[#116A7B] bg-transparent h-20 p-2 m-3 animate-spin duration-[2200] shadow-lg w-20 mx-auto rounded-full"></div>
+            </div>
+          )}
+        </div>
+      )}
+      {recentlyAddedProperties.length > 0 && (
+        <div className="my-10 ">
+          <h1 className="xl:text-[40px] text-4xl mb-16 text-center text-[#116A7B] font-semibold ">
+            Recently Added Properties
+          </h1>
+          {!isRecentlyAddedLoading ? (
+            <div className="xl:mx-24 mx-16 flex flex-row flex-wrap space-x-20">
+              {recentlyAddedProperties.map((card, cardIndex) => (
+                <div
+                  key={cardIndex}
+                  className="w-[20rem] bg-white border-2 border-[#D9D9D9] rounded-xl"
+                >
+                  <div className="p-2 relative">
+                    <Image
+                      width={1000}
+                      height={1000}
+                      src={
+                        card.imageCount > 0
+                          ? `${process.env.NEXT_PUBLIC_SERVER_HOST}/uploads/${card.propertyID}/image-0.png`
+                          : "/assets/user/property-management/no-image.jpg"
+                      }
+                      className="w-[20rem] h-auto object-cover object-center rounded-md overflow-hidden"
+                      // alt={`${}`}
+                    />
+                    <span
+                      // onClick={() =>
+                      //   handleFavouriteList(
+                      //     slideIndex,
+                      //     cardIndex,
+                      //     !favourites[slideIndex][cardIndex]?.status
+                      //   )
+                      // }
+                      className="absolute inset-y-5 left-0 px-5 text-red-600 font-semibold focus:outline-none cursor-pointer"
+                    >
+                      {/* {favourites[slideIndex][cardIndex]?.status ? (
+                  <FaHeart />
+                ) : (
+                  <FaRegHeart />
+                )} */}
+                    </span>
+                    <span className="absolute text-xs inset-y-5 right-0 px-5 text-[#116A7B] font-semibold focus:outline-none cursor-pointer">
+                      {" "}
+                      <VscEye className="inline-flex mb-[1px] mr-1 text-base" />
+                      {card.viewedCount}{" "}
+                    </span>
+                  </div>
+                  <div className="p-2 space-y-1">
+                    <h1 className="text-xl text-[#116A7B]">
+                      {card.propertyType}
+                    </h1>
+                    <h2 className="text-sm text-[#116A7B]">
+                      <strong>
+                        {card?.amenities?.roomDetails?.inputs?.beds
+                          ? card?.amenities?.roomDetails?.inputs?.beds
+                          : "-"}
+                      </strong>{" "}
+                      bd{" "}
+                      <strong>
+                        {card?.amenities?.roomDetails?.inputs?.baths
+                          ? card?.amenities?.roomDetails?.inputs?.baths
+                          : "-"}
+                      </strong>{" "}
+                      ba <strong>{card.area}</strong> Sqft
+                    </h2>
+                    <h3 className="text-sm text-gray-300">
+                      <strong className="text-gray-900">
+                        {card.totalStakes - card.stakesOccupied}
+                      </strong>{" "}
+                      of {card.totalStakes} shares avi.
+                    </h3>
+                    <h4 className="text-xl flex items-start text-[#116A7B]">
+                      <FiMapPin className="inline-flex mt-1 mr-1" />{" "}
+                      {card.addressOfProperty.state},{" "}
+                      {
+                        compCities.getCountryByShort(
+                          card.addressOfProperty.country
+                        ).name
+                      }
+                    </h4>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="bg-white w-full my-6 h-[40rem] max-h-[44rem] overflow-y-auto flex flex-row items-center justify-center">
+              <div className="border-t-4 border-b-4 border-[#116A7B] bg-transparent h-20 p-2 m-3 animate-spin duration-[2200] shadow-lg w-20 mx-auto rounded-full"></div>
+            </div>
+          )}
+        </div>
+      )}
+
+      {featuredProperties.length === 0 && mostViewedProperties.length === 0 && recentlyAddedProperties.length === 0 &&
+        <div className="my-20">
+          <h1 className="xl:text-[4rem] text-4xl text-center text-[#116A7B] font-semibold">No properties yet.</h1>
+        </div>
+      }
     </>
   );
 };
