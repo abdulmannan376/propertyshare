848ad2529b43c71329c99f6fbf608423c915008f
Balaj Ali <balaj.ali707@gmail.com>
2024-07-15 19:31:06 +0500

    offers component created - 2

--- components/user/offers.js
+++ components/user/offers.js
@@ -1,6 +1,25 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 const Offers = () => {
+  const [mySentOffers, setmySentOffers] = useState([]);
+
+  const fetchMySentOffers = async () => {
+    try {
+    } catch (error) {
+      toast.error(error.message, {
+        position: "bottom-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
   return (
     <div className="bg-white w-full my-6 h-[85vh] max-h-[90vh] overflow-y-auto">
       <div className="w-full flex flex-row items-center border-b border-b-[#D9D9D9] pt-1 pb-7 px-14">
