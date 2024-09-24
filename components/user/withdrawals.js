921dcb1369eb216c40f4466df1cb1e32f11c80e5
Balaj Ali <balaj.ali707@gmail.com>
2024-09-24 15:45:15 +0500

    payment history view added in withdrawal managment

--- components/user/withdrawals.js
+++ components/user/withdrawals.js
@@ -80,6 +80,36 @@ const WithdrawalManagement = () => {
     }
   };
 
+  const [paymentHistory, setPaymentHistory] = useState([]);
+  const fetchUserTransactionHistory = async () => {
+    try {
+      setIsWithdrawalsLoading(true);
+
+      const username = JSON.parse(localStorage.getItem("userDetails")).username;
+
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_HOST}/payment/get-user-history/?username=${username}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+
+      const response = await res.json();
+      if (response.success) {
+        setPaymentHistory(response.body);
+      } else {
+        throw new Error(response.message);
+      }
+    } catch (error) {
+      errorAlert(error.message, "");
+    } finally {
+      setIsWithdrawalsLoading(false);
+    }
+  };
+
   useEffect(() => {
     if (activeWithdrawalsTab === "My Withdrawals") {
       fetchWithdrawals("all");
@@ -87,6 +117,8 @@ const WithdrawalManagement = () => {
       fetchWithdrawals("Pending");
     } else if (activeWithdrawalsTab === "Dispatched Withdrawals") {
       fetchWithdrawals("Dispatched");
+    } else if (activeWithdrawalsTab === "Payments History") {
+      fetchUserTransactionHistory();
     }
   }, [activeWithdrawalsTab]);
 
@@ -135,6 +167,7 @@ const WithdrawalManagement = () => {
   const myWithdrawalsRef = useRef(null);
   const pendingWithdrawalsRef = useRef(null);
   const dispatchedWithdrawalsRef = useRef(null);
+  const paymentsHistroyRef = useRef(null);
 
   const handleScrollIntoView = (ref) => {
     ref.current?.scrollIntoView({
@@ -240,6 +273,25 @@ const WithdrawalManagement = () => {
             >
               Dispatched Withdrawals
             </h2>
+          </button>
+        )}
+        {JSON.parse(localStorage.getItem("userDetails")).role !== "admin" && (
+          <button
+            onClick={() => {
+              dispatch(updateActiveWithdrawalTab("Payments History"));
+              handleScrollIntoView(paymentsHistroyRef);
+            }}
+            ref={paymentsHistroyRef}
+          >
+            <h1
+              className={`flex w-52 whitespace-nowrap ${
+                activeWithdrawalsTab === "Payments History"
+                  ? "underline-text"
+                  : "hover-underline-animation"
+              } `}
+            >
+              Payments History
+            </h1>
           </button>
         )}
 
@@ -534,6 +586,118 @@ const WithdrawalManagement = () => {
                           <td className="border border-gray-300 px-4 py-2">
                             {withdrawal.status}
                           </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  ) : (
+                    <h1 className="w-full text-center text-xl p-5">
+                      No Withdrawals
+                    </h1>
+                  )}
+                </table>
+              </div>
+            ) : (
+              <div className="bg-white w-full my-6 h-[20rem] max-h-[44rem] overflow-y-auto flex flex-row items-center justify-center">
+                <div className="border-t-4 border-b-4 border-[#116A7B] bg-transparent h-20 p-2 m-3 animate-spin duration-[2200] shadow-lg w-20 mx-auto rounded-full"></div>
+              </div>
+            )}
+          </div>
+        )}
+        {activeWithdrawalsTab === "Payments History" && (
+          <div>
+            <ViewImageModal
+              isOpen={viewImage}
+              onClose={handleCloseViewImage}
+              imageSrc={viewImageSrc}
+            />
+            {!isWithdrawalsLoading ? (
+              <div className="sm:px-14 px-5 py-6 max-w-screen overflow-x-auto">
+                <table className="min-w-full table-auto border-collapse border border-gray-300">
+                  <thead>
+                    <tr className="bg-gray-100">
+                      <th className="border border-gray-300 px-4 py-2 text-left">
+                        ID
+                      </th>
+                      <th className="border border-gray-300 px-4 py-2 text-left">
+                        Username
+                      </th>
+                      {/* <th className="border border-gray-300 px-4 py-2 text-left">
+                          Name
+                        </th> */}
+
+                      <th className="border border-gray-300 px-4 py-2 text-left">
+                        Payment Type
+                      </th>
+                      <th className="border border-gray-300 px-4 py-2 text-left">
+                        Amount
+                      </th>
+                      <th className="border border-gray-300 px-4 py-2 text-left">
+                        Date
+                      </th>
+                      <th className="border border-gray-300 px-4 py-2 text-left">
+                        Time
+                      </th>
+                      {/* <th className="border border-gray-300 px-4 py-2 text-left">
+                        Reciept
+                      </th> */}
+                    </tr>
+                  </thead>
+                  {paymentHistory.length > 0 ? (
+                    <tbody>
+                      {paymentHistory.map((withdrawal, index) => (
+                        <tr key={index} className="text-sm">
+                          <td className="border border-gray-300 px-4 py-2">
+                            {withdrawal.withdrawalID || withdrawal.paymentID}
+                          </td>
+                          <td className="border border-gray-300 px-4 py-2">
+                            {withdrawal.paymentType === "Credit"
+                              ? withdrawal.userDocID.username
+                              : "admin"}
+                          </td>
+                          {/* <td className="border border-gray-300 px-4 py-2">
+                            {withdrawal.userDocID.name}
+                          </td> */}
+
+                          <td className="border border-gray-300 px-4 py-2">
+                            {withdrawal.paymentType}
+                          </td>
+                          <td
+                            className={`border border-gray-300 px-4 py-2 ${
+                              withdrawal.paymentType === "Credit"
+                                ? "text-green-500"
+                                : "text-red-500"
+                            }`}
+                          >
+                            {withdrawal.paymentType === "Credit"
+                              ? `+${withdrawal.payingAmount}`
+                              : `-${withdrawal.amount}`}
+                          </td>
+                          <td className="border border-gray-300 px-4 py-2">
+                            {new Date(
+                              withdrawal.createdAt
+                            ).toLocaleDateString()}
+                          </td>
+                          <td className="border border-gray-300 px-4 py-2">
+                            {new Date(
+                              withdrawal.createdAt
+                            ).toLocaleTimeString()}
+                          </td>
+                          {/* <td className="border border-gray-300 px-4 py-2">
+                            {withdrawal.status === "Dispatched" && (
+                              <button
+                                type="button"
+                                onClick={() => {
+                                  setViewImageSrc(
+                                    `${process.env.NEXT_PUBLIC_SERVER_HOST}/${withdrawal.imageDir}reciept.png`
+                                  );
+                                  handleOpenViewImageModal();
+                                }}
+                                className="text-blue-500 uppercase underline"
+                              >
+                                View
+                              </button>
+                            )}
+                          </td> */}
                         </tr>
                       ))}
                     </tbody>
